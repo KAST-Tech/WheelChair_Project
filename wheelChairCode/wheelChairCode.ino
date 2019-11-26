35d85ef2a6cb663fe6571ed1f132e154bfacec23
Monmoy042 <monmoykms.16@gmail.com>
2019-11-26 23:08:12 +0600

    Code and Circuit diagram modified

--- wheelChairCode/wheelChairCode.ino
+++ wheelChairCode/wheelChairCode.ino
@@ -1,40 +1,75 @@
-int motorRightA = 8;    // Right Motor-clockwise
-int motorRightB = 9;    // Right Motor-anticlockwise
-int motorLeftA = 11;    // Left Motor-clockwise
-int motorLeftB = 10;    // Left Motor-clockwise
-int trigPin = 12;       // Trig Pin
-int echoPin = 13;       // Echo Pin
-long duration;
-int distance;
-char bt = 0;            //Bluetooth Control
-
+int motorRightA = 8;    //Right Motor-clockwise
+int motorRightB = 9;   //Right Motor-anticlockwise
+int motorLeftA = 11;    //Left Motor-clockwise
+int motorLeftB = 10;    //Left Motor-clockwise
+int trigPin1 = 12;       // Trig Pin
+int echoPin1 = 13;       // Echo Pin
+int light = 5;
+long duration1;
+int distance1;
+char bt = 0;       //Bluetooth Control
+int trigPin2 = 7;       // Trig Pin
+int echoPin2 = 6;       // Echo Pin
+long duration2;
+int distance2;
+int buzzer = 4;
+int pushButton = 3;
 void setup()
 {
   pinMode(motorRightA, OUTPUT);
   pinMode(motorRightB, OUTPUT);
   pinMode(motorRightB, OUTPUT);
   pinMode(motorLeftB, OUTPUT);
-  pinMode(trigPin, OUTPUT);
-  pinMode(echoPin, INPUT);
+  pinMode(trigPin1, OUTPUT);
+  pinMode(echoPin1, INPUT);
+  pinMode(trigPin2, OUTPUT);
+  pinMode(echoPin2, INPUT);
+  pinMode(light, OUTPUT);
+  pinMode(buzzer, OUTPUT);
+  pinMode(pushButton, INPUT_PULLUP);
   Serial.begin(9600);
 }
 void loop()
 {
-  digitalWrite(trigPin, LOW);
+  //Light On Off
+  lightOnOff();
+  //Panic Button
+  panicSound();
+
+  //  Right US
+  digitalWrite(trigPin1, LOW);
   delayMicroseconds(2);
   // Sets the trigPin on HIGH state for 10 micro seconds
-  digitalWrite(trigPin, HIGH);
+  digitalWrite(trigPin1, HIGH);
   delayMicroseconds(10);
-  digitalWrite(trigPin, LOW);
+  digitalWrite(trigPin1, LOW);
   // Reads the echoPin, returns the sound wave travel time in microseconds
-  duration = pulseIn(echoPin, HIGH);
+  duration1 = pulseIn(echoPin1, HIGH);
   // Calculating the distance
-  distance = duration * 0.034 / 2;
+  distance1 = duration1 * 0.034 / 2;
   // Prints the distance on the Serial Monitor
   Serial.print("Distance: ");
-  Serial.println(distance);
+  Serial.println(distance1);
 
-  if (distance <= 20) {
+  // Left US
+
+  digitalWrite(trigPin2, LOW);
+  delayMicroseconds(2);
+  // Sets the trigPin on HIGH state for 10 micro seconds
+  digitalWrite(trigPin2, HIGH);
+  delayMicroseconds(10);
+  digitalWrite(trigPin2, LOW);
+  // Reads the echoPin, returns the sound wave travel time in microseconds
+  duration2 = pulseIn(echoPin2, HIGH);
+  // Calculating the distance
+  distance2 = duration2 * 0.034 / 2;
+  // Prints the distance on the Serial Monitor
+  Serial.print("Distance: ");
+  Serial.println(distance2);
+
+
+
+  if (distance1 <= 20 || distance2 <= 20) {
     //Stop Wheel Chair
     digitalWrite(motorRightA, LOW);
     digitalWrite(motorRightB, LOW);
@@ -47,50 +82,50 @@ void loop()
   }
 }
 
-// All the Controls of the Wheelchair
+// All the Controls of the Wheel Chair
 void control() {
   if (Serial.available() > 0)
   {
     bt = Serial.read();
-    if (bt == 'F')       // Forwards
+    if (bt == 'F')       //move forwards
     {
       digitalWrite(motorRightA, HIGH);
       digitalWrite(motorLeftA,  HIGH);
     }
-    else if (bt == 'B')       // Backwards
+    else if (bt == 'B')       //move backwards
     {
       digitalWrite(motorRightB, HIGH);
       digitalWrite(motorLeftB, HIGH);
     }
-    else if (bt == 'S')     // Stop
+    else if (bt == 'S')     //stop!
     {
       digitalWrite(motorRightA, LOW);
       digitalWrite(motorRightB, LOW);
       digitalWrite(motorLeftA, LOW);
       digitalWrite(motorLeftB, LOW);
     }
-    else if (bt == 'R')    // Right
+    else if (bt == 'R')    //right
     {
       digitalWrite(motorRightA, LOW);
       digitalWrite(motorRightB, LOW);
       digitalWrite(motorLeftA, HIGH);
       digitalWrite(motorLeftB, LOW);
     }
-    else if (bt == 'L')     // Left
+    else if (bt == 'L')     //left
     {
       digitalWrite(motorRightA, HIGH);
       digitalWrite(motorRightB, LOW);
       digitalWrite(motorLeftA, LOW);
       digitalWrite(motorLeftB, LOW);
     }
-    else if (bt == 'I')    // Forward Right
+    else if (bt == 'I')    //forward right
     {
       digitalWrite(motorRightA, HIGH);
       digitalWrite(motorRightB, LOW);
       digitalWrite(motorLeftA, LOW);
       digitalWrite(motorLeftB, HIGH);
     }
-    else if (bt == 'G')    // Forward Left
+    else if (bt == 'G')    //forward left
     {
       digitalWrite(motorRightA, LOW);
       digitalWrite(motorRightB, HIGH);
@@ -100,3 +135,20 @@ void control() {
   }
 }
 
+void lightOnOff() {
+
+  if (bt == 'O') {
+    digitalWrite(light, HIGH);
+  }
+  else if (bt == 'o') {
+    digitalWrite(light, LOW);
+  }
+}
+void panicSound() {
+  int val = digitalRead(pushButton);
+  if (val == LOW) {
+    digitalWrite(buzzer, HIGH);
+  } else {
+    digitalWrite(buzzer, LOW);
+  }
+}
